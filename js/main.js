d22249e39dacd70a822404a1975d2b9145eb719d
KHyeonggi <giroro3946@gmail.com>
2024-06-05 11:39:45 +0900

    css+ js+

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+const box = document.querySelector("#ani3");
+
+document.getElementById("start").addEventListener("click", function(){
+  box.style.animationPlayState = "running";
+});
+
+document.getElementById("paused").addEventListener("click", function(){
+  box.style.animationPlayState = "paused";
+})
